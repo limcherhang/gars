e9f40f53e8fce2af06b0307bbf89a7769e7ad96a
cherhanglim <cherhanglim0227@gmail.com>
2023-03-08 16:16:29 +0800

    Update code 3/8

---
+++ func.py
@@ -0,0 +1,104 @@
+import logging
+from reportlab.pdfgen import canvas
+from reportlab.lib import units
+from typing import Union
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+logger = logging.getLogger(__name__)
+
+def generate_pdf_receipt(
+    filename: str,
+    company_name: str,
+    company_address: str,
+    company_phone: str,
+    receipt_number: str,    
+    receipt_date: str,
+    descriptions: dict,
+    angle: Union[int, float],
+    images: str,
+    font: str,
+    client_name: str,
+    client_num: str,
+    logger: logging.Logger,
+):
+    total = 0
+
+    # 創建一個PDF檔案
+    pdf_file = canvas.Canvas(f"{filename}.pdf")
+
+    # 設置字型
+    pdf_file.setFont(font, 16)
+
+    # 寫入收據信息
+    weight = 750
+    logger.info(f"公司名稱 weight: {weight}")
+    pdf_file.drawString(50, weight, company_name)
+    pdf_file.setFont(font, 12)
+    pdf_file.drawString(400, weight, "收据编号: {}".format(receipt_number))
+
+    weight -= 15
+    logger.info(f"公司地址 weight: {weight}")
+    pdf_file.drawString(50, weight, company_address)
+    pdf_file.drawString(400, weight, "日期: {}".format(receipt_date))
+
+    weight -= 15
+    logger.info(f"公司電話 weight: {weight}")
+    pdf_file.drawString(50, weight, company_phone)
+
+    weight -= 20
+    logger.info(f"客戶名稱 weight: {weight}")
+    pdf_file.drawString(50, weight, f"名称: {client_name}")
+    pdf_file.drawString(400, weight, f"会员编号: {client_num}")
+
+    weight -= 10
+    logger.info(f"線1 weight: {weight}")
+    pdf_file.line(50, weight, 550, weight)
+
+    # 寫入費用信息
+    weight -= 20
+    logger.info(f"內容 weight: {weight}")
+    pdf_file.drawString(50, weight, "内容")
+    pdf_file.drawString(250, weight, "价格")
+    pdf_file.drawString(350, weight, "税")
+
+    weight -= 10
+    logger.info(f"線2 weight: {weight}")
+    pdf_file.line(50, weight, 550, weight)
+    weight -= 20
+    logger.info(f"第1筆 weight: {weight}")
+    for i, (_, descrip) in enumerate(descriptions.items()):
+        description = descrip["description"]
+        price = descrip["price"]
+        tax = descrip["tax"]
+        pdf_file.drawString(50, weight, description)
+        pdf_file.drawString(250, weight, f"{price:,.2f}")
+        pdf_file.drawString(350, weight, f"{tax:,.0%}")
+        total += price + price * tax
+
+        weight -= 20
+        if i == len(descriptions) - 1:
+            logger.info(f"第{i+2}筆 weight: {weight}")
+        else:
+            logger.info(f"線3 weight: {weight}")
+
+    pdf_file.line(50, weight, 550, weight)
+    weight -= 20
+    logger.info(f"總計 weight: {weight}")
+    pdf_file.drawString(50, weight, "总计: ")
+    pdf_file.drawString(250, weight, f"{total:,.2f}")
+
+    # 设定浮水印
+    pdf_file.rotate(angle)
+    # 设置透明度
+    pdf_file.setFillAlpha(0.4)
+    pdf_file.drawImage(images, 9 * units.cm, 23 * units.cm, 3 * units.cm, 3 * units.cm)
+
+    # 完成並關閉PDF檔案
+    pdf_file.showPage()
+    pdf_file.save()
+
+
+def df_to_dict(df: DataFrame):
+    return df.to_dict("records")
+
---
+++ generate_receipt.py
@@ -0,0 +1,56 @@
+import logging
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+from func import generate_pdf_receipt, df_to_dict
+
+logger = logging.getLogger()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    encoding="utf-8",
+    filename="generateReceipt.log",
+)
+
+font = "simsun"
+
+# 設置中文字型
+pdfmetrics.registerFont(TTFont("simsun", "simsun.ttf"))
+
+# 參數
+angle = 0  # 旋轉角度
+images = "log.png"  # 圖片名稱
+
+# 設置收據信息
+client_name = "小明先生"
+client_num = "A12345"
+company_name = "全球针灸紧急救援传统与辅助医学学会"
+company_address = "1234 Main St, Anytown, USA"
+company_phone = "(555) 555-5555"
+receipt_number = "001-002-0001234"
+receipt_date = "2023-03-06"
+filename = "receipt"
+
+# 設置費用信息
+descriptions = {
+    "1": {"description": "服務費", "price": 1000.00, "tax": 0.05},
+    "2": {"description": "會費", "price": 200.00, "tax": 0.05},
+}
+
+logger.info("Prepare to generate pdf receipt")
+generate_pdf_receipt(
+    filename,
+    company_name,
+    company_address,
+    company_phone,
+    receipt_number,
+    receipt_date,
+    descriptions,
+    angle,
+    images,
+    font,
+    client_name,
+    client_num,
+    logger,
+)
